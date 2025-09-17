d66547c3e4a41a880f3a6c44953c48ff8387d473
sharabh mishra <sharabhmishra15@gmail.com>
2025-09-18 00:33:49 +0530

    added basic backend for prompt-response generation

--- src/app/api/prompts/route.js
+++ src/app/api/prompts/route.js
@@ -1,13 +1,24 @@
 import { NextResponse } from 'next/server';
 import connectDB from '@/app/lib/dbconnect';
+import {generateText} from 'ai';
+import { createGoogleGenerativeAI } from '@ai-sdk/google';
+
+const googleAI = createGoogleGenerativeAI({
+  apiKey: process.env.GOOGLE_API_KEY,
+});
 
 export async function POST(request) {
     try {
       await connectDB();
       const body = await request.json();
       console.log(body);
-      return NextResponse.json({ message: "This is a prompts route" }, { status: 200 })
+      const response = await generateText({
+        model: googleAI("gemini-2.0-flash-001"),
+        prompt: body.prompt,
+      });
+      return NextResponse.json({ message: response.text }, { status: 200 })
+    // return NextResponse.json({ message: "This is a prompts route" }, { status: 200 })
     } catch (error) {
-      return NextResponse.json({ message: "Prompts failed" }, { status: 500 })
+      return NextResponse.json({ message: "Prompts failed with error: " + error }, { status: 500 })
     }
   }
